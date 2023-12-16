4e38e2cc6f3ba072a414b783b8e7e35bf327af69
Paulo Carvalho <phcarvalhodev@gmail.com>
2023-12-16 13:20:37 -0300

    added api routes

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/phcarvalho/gostore
 
 go 1.21
+
+require github.com/gorilla/mux v1.8.1
---
+++ service.go
@@ -0,0 +1,73 @@
+package main
+
+import (
+	"errors"
+	"io"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+func keyValuePutHandler(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	key := vars["key"]
+
+	value, err := io.ReadAll(r.Body)
+	defer r.Body.Close()
+
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	err = Put(key, string(value))
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.WriteHeader(http.StatusCreated)
+}
+
+func keyValueGetHandler(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	key := vars["key"]
+
+	value, err := Get(key)
+	if errors.Is(err, ErrorNoSuchKey) {
+		http.Error(w, err.Error(), http.StatusNotFound)
+		return
+
+	}
+
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.Write([]byte(value))
+}
+
+func keyValueDeleteHandler(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	key := vars["key"]
+
+	err := Delete(key)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+}
+
+func main() {
+	r := mux.NewRouter()
+
+	r.HandleFunc("/v1/{key}", keyValueGetHandler).Methods(http.MethodGet)
+	r.HandleFunc("/v1/{key}", keyValuePutHandler).Methods(http.MethodPut)
+	r.HandleFunc("/v1/{key}", keyValueDeleteHandler).Methods(http.MethodDelete)
+
+	log.Fatal(http.ListenAndServe(":4000", r))
+}
